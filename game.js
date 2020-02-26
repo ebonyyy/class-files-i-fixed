5a479afaaa870305dd6fc535bed531f97e100239
ebonyyy <38299558+ebonyyy@users.noreply.github.com>
2020-02-26 17:58:11 -0500

    Add files via upload

---
+++ game.js
@@ -0,0 +1,37 @@
+
+// state
+let hero = new Hero()
+let keyPressed = {}
+
+// setup a function to run whenever a key is pressed
+window.addEventListener("keydown", event => {
+  keyPressed[event.code] = true
+})
+window.addEventListener("keyup", event => {
+  keyPressed[event.code] = false
+})
+
+function loop() {
+  console.log(keyPressed)
+  if (keyPressed["ArrowUp"]) {
+    hero.jump()
+  }
+  if (keyPressed["ArrowLeft"]) {
+    hero.moveLeft()
+  }
+  if (keyPressed["ArrowRight"]) {
+    hero.moveRight()
+  }
+
+  hero.step()
+
+  // draw everything
+  ERASE()
+  hero.draw()
+
+  // run loop again!
+  setTimeout(loop, 1000 / 60)
+}
+
+// run loop once on startup to get it started
+loop()
---
+++ hero.js
@@ -0,0 +1,80 @@
+
+let heroStand= new Sprite("images/heros/blue/alienBlue_stand.png")
+let heroJump= new Sprite("images/heros/blue/alienBlue_jump.png")
+
+class Hero {
+  constructor() {
+    this.x = GRIDSIZE
+    this.y = GRIDSIZE
+    this.width = GRIDSIZE
+    this.height = GRIDSIZE*2
+
+    this.dy = 0
+    this.airborne = true
+  }
+  jump() {
+    if (this.airborne) {
+      return
+    }
+    this.dy = - GRIDSIZE/2
+    this.airborne = true
+  }
+  moveLeft() {
+    this.x = this.x - GRIDSIZE/10
+  }
+  moveRight() {
+    this.x = this.x + GRIDSIZE/10
+  }
+  step() {
+    // acceleration falling speed
+    this.dy = this.dy + GRIDSIZE/60
+    if (this.dy > GRIDSIZE) {
+      this.dy = GRIDSIZE - 1
+    }
+
+    // apply speed to position
+    this.y = this.y + this.dy
+
+    // check if hero is in the ground
+    if (this.y > CANVAS.height) {
+      this.y = CANVAS.height
+      this.airborne = false
+    }
+  }
+  draw() {
+    CTX.fillStyle = 'black'
+    CTX.beginPath()
+    CTX.arc(
+      this.x,
+      this.y,
+      this.width,
+      0,
+      2 * Math.PI,
+    )
+    CTX.fill()
+
+    let imageToDraw = heroStand
+    if(this.airborne){
+      imageToDraw = heroJump
+    }
+    imageToDraw.draw(
+      this.x - this.width / 2,
+      this.y - this.height,
+      this.width,
+      this.height
+    )
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
